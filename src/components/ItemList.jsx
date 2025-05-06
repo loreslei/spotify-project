14550f578c911b1a102b54f18a879863209584db
Lorenna Aguiar Nunes <133262137+loreslei@users.noreply.github.com>
2025-05-06 20:40:14 -0300

    added map function

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import SingleItem from './SingleItem'
-const ItemList = ({title, items}) => {
+import React from "react";
+import SingleItem from "./SingleItem";
+const ItemList = ({ title, items }) => {
   return (
-      <div className="item-list">
-        
-        <div className="item-list__header">
+    <div className="item-list">
+      <div className="item-list__header">
         <h2>{title} Populares</h2>
-          <a className="main__link" href="">
-            Mostrar Tudo
-          </a>
-        </div>   
-
-        <div className="item-list__container">
-          <SingleItem />
-          <SingleItem />
-          <SingleItem />
-          <SingleItem />
-        </div>
+        <a className="main__link" href="">
+          Mostrar Tudo
+        </a>
       </div>
-  )
-}
 
-export default ItemList
+      <div className="item-list__container">
+        {Array(items)
+          .fill()
+          .map(() => (
+            <SingleItem />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default ItemList;
