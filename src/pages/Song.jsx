f346fd56dc53c07513059ec1d3312e52e72a9cfa
Lorenna Aguiar Nunes <133262137+loreslei@users.noreply.github.com>
2025-05-11 11:44:27 -0300

    added player component and song page

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
-
+import React from "react";
+import Player from "../components/Player";
 const Song = () => {
   return (
-    <div>
-      
+    <div className="song">
+      <div className="song__container">
+        <div className="song__image-container">
+          <img
+            src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24"
+            alt="Imagem da Música X"
+          />
+        </div>
+      </div>
+      <div className="song__bar">
+        <div className="song__artist-image">
+          <img
+          width={75}
+          height={75}
+            src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+            alt="Imagem do Artista Y"
+          />
+        </div>
+        <Player />
+        <div>
+          <p className="song__name">Última Saudade - Ao vivo</p>
+          <p className="">Jorge e Mateus</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Song
+export default Song;
