833cd78677053b5771fe83d87d85b3ad33eb2a04
Lorenna Aguiar Nunes <133262137+loreslei@users.noreply.github.com>
2025-05-14 11:47:34 -0300

    changed route

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import axios from "axios";
 
-const {NODE_ENV} = process.env;
+/* const {NODE_ENV} = process.env;
 const URL = NODE_ENV === 'development' ? "http://localhost:3000/api" : "/api";
+ */
 
+const URL = "https://spotify-project-dcc6.onrender.com/api"
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
 
